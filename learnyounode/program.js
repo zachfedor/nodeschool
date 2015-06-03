f7cd5f5b5d2d76bfcc678cf5d5fabb020399c449
zachfedor <zachfedor@gmail.com>
2015-06-03 15:58:06 -0400

    2 of 13 for learnyounode

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,1 +1,9 @@
-console.log( "HELLO WORLD" );
+if( process.argv.length > 2 ) {
+    var x = 0;
+
+    for( var i = 2; i < process.argv.length; i++ ) {
+        x += Number( process.argv[ i ]);
+    }
+
+    console.log( x );
+}
