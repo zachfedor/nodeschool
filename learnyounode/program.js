0b280ab0bab6f570664901f35fbdb4c40be5f277
zachfedor <zachfedor@gmail.com>
2015-06-03 22:56:52 -0400

    4 of 13 learnyounode refactored

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,13 +1,10 @@
 var fs = require('fs');
 
-fs.readFile( process.argv[ 2 ], 'utf8', printNewLines );
-
-function printNewLines( err, data ) {
+fs.readFile( process.argv[ 2 ], 'utf8', function( err, data ) {
     if( err ) {
         console.log( err );
     } else {
-        var lines = data.toString().split( '\n' );
-
+        var lines = data.split( '\n' );
         console.log( lines.length - 1 );
     }
-}
+});
