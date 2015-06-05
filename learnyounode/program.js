9b0499d130865cc716625610ec12632c46db82f2
zachfedor <zachfedor@gmail.com>
2015-06-05 16:33:14 -0400

    8 of 13 node - http collect

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,7 +1,10 @@
 var http = require( 'http' );
+var concatStream = require( 'concat-stream' );
 
 http.get( process.argv[ 2 ], function( response ) {
-    response.setEncoding( "utf8" );
-    response.on( "data", console.log );
-    response.on( "error", console.error );
+    //response.setEncoding( "utf8" );
+    response.pipe( concatStream( function( data ) {
+        console.log( data.toString().length );
+        console.log( data.toString());
+    }));
 });
