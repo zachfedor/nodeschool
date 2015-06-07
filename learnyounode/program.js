bbe28f886273d6caf9082c094a1e6a776252d9c8
zachfedor <zachfedor@gmail.com>
2015-06-07 18:17:01 -0400

    9 of 13 node - juggling async

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,10 +1,32 @@
 var http = require( 'http' );
 var concatStream = require( 'concat-stream' );
+var content = {};
+
+function printContent() {
+    if( content.one && content.two && content.three ) {
+        console.log( content.one );
+        console.log( content.two );
+        console.log( content.three );
+    }
+}
 
 http.get( process.argv[ 2 ], function( response ) {
-    //response.setEncoding( "utf8" );
     response.pipe( concatStream( function( data ) {
-        console.log( data.toString().length );
-        console.log( data.toString());
+        content.one = data.toString();
+        printContent();
+    }));
+});
+
+http.get( process.argv[ 3 ], function( response ) {
+    response.pipe( concatStream( function( data ) {
+        content.two = data.toString();
+        printContent();
+    }));
+});
+
+http.get( process.argv[ 4 ], function( response ) {
+    response.pipe( concatStream( function( data ) {
+        content.three = data.toString();
+        printContent();
     }));
 });
