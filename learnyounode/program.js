8199d6aacaae9a50b7cab99c987a8243e63795f0
zachfedor <zachfedor@gmail.com>
2015-06-08 15:56:30 -0400

    10 of 13 node - time server

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,27 +1,8 @@
-var http = require( 'http' );
-var concatStream = require( 'concat-stream' );
-var content = [];
-var count = 0;
+var net = require( 'net' );
+var moment = require( 'moment' );
+var data = moment().format( 'YYYY-MM-DD HH:mm' ) + "\n";
 
-function printContent() {
-    for( var i = 0; i < 3; i++ ) {
-        console.log( content[ i ] );
-    }
-}
-
-function getContent( index ) {
-    http.get( process.argv[ 2 + index ], function( response ) {
-        response.pipe( concatStream( function( data ) {
-            content[ index ] = data.toString();
-            count++;
-
-            if( count == 3 ) {
-                printContent();
-            }
-        }));
-    });
-}
-
-for( var i = 0; i < 3; i++ ) {
-    getContent( i );
-}
+var server = net.createServer( function( socket ) {
+    socket.end( data );
+});
+server.listen( process.argv[ 2 ]);
