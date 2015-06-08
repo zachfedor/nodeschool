5c897840858bf975aca7c74cef8f38de837c3f00
zachfedor <zachfedor@gmail.com>
2015-06-08 17:35:06 -0400

    11 of 13 node - refactored http server

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -2,6 +2,8 @@ var fs = require( 'fs' );
 var http = require( 'http' );
 
 var server = http.createServer( function( request, response ) {
+    response.writeHead( 200, { 'content-type': 'text/plain' });
+
     fs.createReadStream( process.argv[ 3 ]).pipe( response );
 });
 
