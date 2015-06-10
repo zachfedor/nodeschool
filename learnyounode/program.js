71e34a30f74bfee748f0a1fdd7a2b684471686a2
zachfedor <zachfedor@gmail.com>
2015-06-10 14:35:12 -0400

    13 of 13 node - JSON API server

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,14 +1,20 @@
 var http = require( 'http' );
-var map = require( 'through2-map' );
+var url = require( 'url' );
 
 var server = http.createServer( function( request, response ) {
-    if( request.method != 'POST' ) {
-        return response.end( 'Send via POST, please\n' );
-    }
+    var path = url.parse( request.url, true );
+    var date = new Date( path.query.iso );
 
-    request.pipe( map( function( chunk ) {
-        return chunk.toString().toUpperCase();
-    })).pipe( response );
+    response.writeHead( 200, { 'Content-type': 'application/json' });
+
+    if( path.pathname == '/api/parsetime' ) {
+        response.end( JSON.stringify({ hour: date.getHours(), minute: date.getMinutes(), second: date.getSeconds() }));
+    } else if( path.pathname == '/api/unixtime' ) {
+        response.end( JSON.stringify({ unixtime: date.getTime() }));
+    } else {
+        response.writeHead( 404 );
+        response.end()
+    }
 });
 
 server.listen( Number( process.argv[ 2 ]));
