1f2257c58a438d8eb2cbcbad3eecddf61faeda66
zachfedor <zachfedor@gmail.com>
2015-06-16 11:40:43 -0400

    4 of 15 stream - transform

--- stream-adventure/program.js
+++ stream-adventure/program.js
@@ -1,3 +1,14 @@
 var fs = require( 'fs' );
+var through = require( 'through2' );
+var stream = through( write, end );
 
-process.stdin.pipe( process.stdout );
+function write( buffer, encoding, next ) {
+    this.push( buffer.toString().toUpperCase() );
+    next();
+}
+
+function end( done ) {
+    done();
+}
+
+process.stdin.pipe( stream ).pipe( process.stdout );
